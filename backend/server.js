c69c847ca502b61195fc0ea74b825c6baa9bc2c5
Amar Srivastava <amarsrivastava68@gmail.com>
2024-09-30 11:46:10 +0530

    started working on frontend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
-
-app.listen(5000  , console.log('server started on 5000'))
-
+const notes = require('./data/notes')
+const dotenv = require ('dotenv')
+dotenv.config()
+const PORT = process.env.PORT || 5000;
+app.listen(
+    PORT,
+    console.log(
+      `Server running on port ${PORT}..`
+    )
+  );
 app.get('/' ,(req , res)=>{res.send('API is running')})
+app.get('/api/notes' , (req , res ) => {
+    res.json(notes)
+})
+app.get('/api/notes/:id' , (req , res ) => {
+     
+    const note = notes.find(n => (n._id) === (req.params.id))
+    console.log(req.params)
+    res.json(note)
+})
