56964a16a2c01d4c7a545a21a876c190dcdf217e
Amar Srivastava <amarsrivastava68@gmail.com>
2024-10-01 10:44:37 +0530

    added the wrapper component for main screen and added browser Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,24 @@
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import LandingPage from './screens/LandingPage/LandingPage';
-
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import MyNotes from './screens/myNotes/MyNotes';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Header/>
       <main >
-      <LandingPage />
+        <Routes>
+          <Route path='/' element ={<LandingPage/>}/>
+          <Route path='/mynotes' element ={<MyNotes/>}/>
+
+        </Routes>
+     
       </main>
       <Footer/>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/myNotes/MyNotes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import MainScreen from '../../components/MainScreen'
+const MyNotes = () => {
+  return (
+    <MainScreen title='welcome back amar srivastava'>these are my notes</MainScreen>
+  )
+}
+
+export default MyNotes
