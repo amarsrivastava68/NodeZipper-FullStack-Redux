ee7f9986766d8bf81d271d2dee0b6fbdb4a6ae50
Amar Srivastava <amarsrivastava68@gmail.com>
2024-10-01 15:42:47 +0530

    added collapsible accordions to show notes data

--- frontend/src/screens/myNotes/MyNotes.js
+++ frontend/src/screens/myNotes/MyNotes.js
@@ -1,9 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Badge, Accordion } from "react-bootstrap";
+import { useAccordionButton } from 'react-bootstrap/AccordionButton';
 import MainScreen from "../../components/MainScreen";
 import notes from "./../../data/notes";
+
+function CustomToggle({ children, eventKey }) {
+    const decoratedOnClick = useAccordionButton(eventKey, () =>
+      console.log('totally custom!'),
+    );
+  
+    return (
+      <button
+        type="button"
+        style={{ border : 0, backgroundColor : 'transparent' , borderRadius : 6 , padding: 4 }}
+        onClick={decoratedOnClick}
+      >
+        {children}
+      </button>
+    );
+  }
+
 const MyNotes = () => {
+    
+
+
   const deleteHandler = (id) => {
     if (window.confirm("are you sure ...")) {
     }
@@ -16,45 +37,52 @@ const MyNotes = () => {
         </Button>
       </Link>
       {notes.map((note) => (
-        <Card style={{ margin: 10 }}>
-          <Card.Header style={{ display: "flex" }}>
-            <span
-              style={{
-                color: "black",
-                textDecoration: "none",
-                flex: 1,
-                cursor: "pointer",
-                alignSelf: "center",
-                fontSize: 18,
-              }}
-            >
-              {note.title}
-            </span>
-            <div>
-              <Button href={`/note/${note._id}`}>Edit</Button>
-              <Button
-                onClick={() => {
-                  deleteHandler(note._id);
+        <Accordion>
+          <Card style={{ margin: 10 }}>
+            <Card.Header style={{ display: "flex" }}>
+              <span
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  flex: 1,
+                  cursor: "pointer",
+                  alignSelf: "center",
+                  fontSize: 18,
                 }}
-                style={{ marginLeft: 8 }}
-                variant="danger"
               >
-                Delete
-              </Button>
-            </div>
-          </Card.Header>
-          <Card.Body>
-            <blockquote className="blockquote mb-0">
-              <p>
-                lorem ipsum asd aswer dfg ghj qer zxcv asd rwe rty sd xdfg w wef
-                wet 3r dfh 43 sdg sdf s a ante . 
-              </p>
-              <footer className="blockquote-footer">
-                Someone in side 
-              </footer>
-            </blockquote>
-          </Card.Body>
-        </Card>
+                 <CustomToggle eventKey="0">{note.title}</CustomToggle>
+              </span>
+              <div>
+                <Button href={`/note/${note._id}`}>Edit</Button>
+                <Button
+                  onClick={() => {
+                    deleteHandler(note._id);
+                  }}
+                  style={{ marginLeft: 8 }}
+                  variant="danger"
+                >
+                  Delete
+                </Button>
+              </div>
+            </Card.Header>
+            <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              <h4>
+                <Badge variant="success" style={{ color: "white" }}>
+                  category - {note.category}
+                </Badge>
+              </h4>
+
+              <blockquote className="blockquote mb-0">
+                <p>{note.content}</p>
+                <footer className="blockquote-footer">
+                  created on - 10th July 2021
+                </footer>
+              </blockquote>
+            </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
       ))}
     </MainScreen>
   );
