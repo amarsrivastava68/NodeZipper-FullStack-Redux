ad78f975ca3466c5f6929857139f4423634e845b
Amar Srivastava <amarsrivastava68@gmail.com>
2024-10-03 14:30:50 +0530

    created userModel , userController and userRoute for registering new user

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,21 @@
-const express = require('express')
-const app = express()
-const notes = require('./data/notes')
-const dotenv = require ('dotenv')
-const connectDB = require('./db')
-dotenv.config()
-connectDB()
+const express = require("express");
+const app = express();
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const connectDB = require("./db");
+dotenv.config();
+
+connectDB();
+
 const PORT = process.env.PORT || 5000;
-app.listen(
-    PORT,
-    console.log(
-      `Server running on port ${PORT}..`
-    )
-  );
-app.get('/' ,(req , res)=>{res.send('API is running')})
-app.get('/api/notes' , (req , res ) => {
-    res.json(notes)
-})
-app.get('/api/notes/:id' , (req , res ) => {
-     
-    const note = notes.find(n => (n._id) === (req.params.id))
-    console.log(req.params)
-    res.json(note)
-})
+app.listen(PORT, console.log(`Server running on port ${PORT}..`));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.use("/api/users", userRoutes);
